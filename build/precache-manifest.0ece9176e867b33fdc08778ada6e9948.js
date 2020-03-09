38991972aefeea032d39daf5408e3a764a8b699d
darshan405 <dshivakumar@qwinix.io>
2020-03-09 07:49:05 +0530

    added build folder

---
+++ build/precache-manifest.0ece9176e867b33fdc08778ada6e9948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9d3ae3554826edebbbe6dba600c7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16076d6b8bdd9c8ad8b9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7c5dc555ee49d528e44",
+    "url": "/static/js/2.ff2f80c8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ff2f80c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16076d6b8bdd9c8ad8b9",
+    "url": "/static/js/main.81ab5486.chunk.js"
+  },
+  {
+    "revision": "6811edc2838838b71820",
+    "url": "/static/js/runtime-main.de856fc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
